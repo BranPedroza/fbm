42021f38bfa0debeda6b49d3678f51dcf2fa282c
bran22 <bran22elp@gmail.com>
2019-10-26 18:40:52 -0500

    primerrepo

---
+++ fvm/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+
+
+urlpatterns = [
+    path('dashboard/',include('dashboard.urls')),
+    path('usuarios/',include('usuarios.urls'))
+    # Agregar app de registro
+]
