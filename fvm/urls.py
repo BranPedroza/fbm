5ae3f7c9715754a0b2831c938c92836d6999b6da
bran22 <bran22elp@gmail.com>
2019-10-26 23:13:52 -0500

    para angel

--- fvm/urls.py
+++ fvm/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path, include
 
 
+
 urlpatterns = [
     path('dashboard/',include('dashboard.urls')),
-    path('usuarios/',include('usuarios.urls'))
-    # Agregar app de registro
+    path('usuarios/',include('usuarios.urls')),
 ]
--- usuarios/urls.py
+++ usuarios/urls.py
@@ -2,6 +2,14 @@ from django.urls import path
 
 from . import views
 
+from django.contrib import admin
+from django.urls import path
+from usuarios import views
+
 urlpatterns = [
-    path('', views.index, name= 'index'),
+    path('', views.welcome),
+    path('register', views.register),
+    path('login', views.login),
+    path('logout', views.logout),
+    path('admin/', admin.site.urls),
 ]
